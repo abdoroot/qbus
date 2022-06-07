4c57eaf3084f80aa4e2125bea8fa34b526020f75
Abdelhadi Mohamed <abd.200930@gmail.com>
2022-06-07 13:20:41 +0200

    fix profile pic

--- resources/views/guest/layouts/nav.blade.php
+++ resources/views/guest/layouts/nav.blade.php
@@ -86,7 +86,7 @@
                         aria-label="toggle profile dropdown">
                         <div class="w-8 h-8 overflow-hidden border-2 border-gray-400 rounded-full">
                             <a href="{{ route('profile.index') }}">
-                                <img src="{{ is_file(asset('images/users/' . Auth::user()->image)) ? asset('images/users/' . Auth::user()->image):asset('images/users/default-user-image.png') }}"class="object-cover w-full h-full" alt="avatar">
+                                <img src="{{ file_exists(public_path('images/users/' . Auth::user()->image)) ? asset('images/users/' . Auth::user()->image):asset('images/users/default-user-image.png') }}"class="object-cover w-full h-full" alt="avatar">
                             </a>
                         </div>
                         <h3 class="mx-2 text-lg font-medium text-gray-700 dark:text-gray-200 md:hidden">{{ Auth::user()->name }}</h3>
--- resources/views/user/profile/menu.blade.php
+++ resources/views/user/profile/menu.blade.php
@@ -1,6 +1,6 @@
 <div class="flex flex-col items-center mt-6 -mx-2">
 
-        <img class="object-cover w-24 h-24 mx-2 rounded-full" src="{{ asset('images/users/' . $user->image) }}" alt="avatar">
+        <img class="object-cover w-24 h-24 mx-2 rounded-full"  src="{{ file_exists(public_path('images/users/' . $user->image)) ? asset('images/users/' . $user->image):asset('images/users/default-user-image.png') }}" alt="avatar">
         <h4 class="mx-2 mt-2 font-medium text-gray-800 dark:text-gray-200 hover:underline">{{ $user->name }}</h4>
         <p class="mx-2 mt-1 text-sm font-medium text-gray-600 dark:text-gray-400 hover:underline">  {{ $user->phone }}</p>
     </div>
